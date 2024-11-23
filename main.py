dcdd5f13d01d0a34d8dc1d0c91890d13dbc87841
Tom Mount <tmountjr@gmail.com>
2024-11-23 16:36:54 -0500

    now returns array of added jobs since manifest was last saved

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
+import re
 import json
 import requests
+from deepdiff import DeepDiff
 
-CAREERS_URL = "https://www.fastly.com/about/careers/current-openings"
+CAREERS_URL = "https://api.greenhouse.io/v1/boards/fastly/embed/departments"
 
-# Data is structured like this:
-# div#job-list is the main list of jobs
-#   child divs[data-testid="department"] nodes are categories
-#     within each category, h3 contains the category name
-#     child divs after h3 are all the open jobs
-#       within each child div,
-#           a element has:
-#             * href to JD
-#             * name of position is text content
-#           p element has location
-#
-# potential JSON structure in manifest.json
-#
-# Dictionary comparison as sets: https://stackoverflow.com/a/60935162
-#
-# Process:
-# 1. read in manifest.json as dictionary
-# 2. scrape CAREERS_URL
-# 3. parse with bs4
-# 4. pick out div#job-list
-# 5. crawl through div and generate a new dict with the current data
-# 6. do a deep set comparison between the manifest and what was crawled. Print any additions.
-# 7. save the new crawl response to manifest.json.
+session = requests.session()
 
-with open("./manifest.json", "r", encoding="utf-8") as manifest:
-    old_manifest = json.loads(manifest.read())
+# Load manifest.json.
+with open("./manifest.json", "r", encoding="utf-8") as manifest_handle:
+    manifest = json.loads(manifest_handle.read())
+
+try:
+    response = session.request("get", CAREERS_URL)
+    response.raise_for_status()
+    new_data = json.loads(response.text)
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching URL: {e}")
+
+def simplify_job(j):
+    return {
+        "url": j["absolute_url"],
+        "location": j["location"]["name"],
+        "title": j["title"]
+    }
+
+# Create a hash table where the key is the top-level department id
+# and the value is a dictionary of name and jobs
+postings = {}
+for department in new_data["departments"]:
+    dept_id = department["id"]
+    parent_id = department["parent_id"]
+
+    if parent_id is None:
+        if dept_id in postings:
+            # stub, overwrite name and merge jobs
+            postings[dept_id]["name"] = department["name"]
+            postings[dept_id]["jobs"] += [ simplify_job(x) for x in department["jobs"] ]
+        else:
+            # new entry
+            postings[dept_id] = {
+                "name": department["name"],
+                "jobs": [ simplify_job(x) for x in department["jobs"] ]
+            }
+    else:
+        if parent_id in postings:
+            # update existing parent department jobs
+            postings[parent_id]["jobs"] += [ simplify_job(x) for x in department["jobs"] ]
+        else:
+            # stub out new entry for parent department
+            postings[parent_id] = {
+                "name": "PENDING",
+                "jobs": [ simplify_job(x) for x in department["jobs"] ]
+            }
+
+diff = DeepDiff(
+    manifest,
+    json.loads(json.dumps(postings)),
+    threshold_to_diff_deeper=0.8
+)
+if "iterable_item_added" in diff:
+    added = diff["iterable_item_added"]
+
+    new_jobs_posted = []
+    for key in added.keys():
+        chunks = re.findall(r"'(.*?)'|\[(\d+)\]", key)
+        dept_id, key, index = [ item for sublist in chunks for item in sublist if item ]
+        dept_id = int(dept_id)
+        index = int(index)
+        job = postings[dept_id][key][index]
+        dept_name = postings[dept_id]["name"]
+        new_jobs_posted.append(
+            f'New job posted in "{dept_name}": {job["title"]} based in {job["location"]}.'
+        )
+
+    print(new_jobs_posted)
+else:
+    print("no new jobs posted.")
